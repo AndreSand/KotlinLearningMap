6c4f77ede30cdc473ca419abe55bc4156205dcb1
Andres Sandoval <android.sjsu@gmail.com>
2017-10-17 13:34:04 -0700

    test codecov report

---
+++ app/src/androidTest/java/com/kotlinmap/andres/mapapp/FillinFormTest.java
@@ -0,0 +1,89 @@
+package com.kotlinmap.andres.mapapp;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class FillinFormTest {
+
+    @Rule
+    public ActivityTestRule<FillinForm> mActivityTestRule = new ActivityTestRule<>(FillinForm.class);
+
+    @Test
+    public void fillinFormTest() {
+
+        ViewInteraction appCompatEditText9 = onView(
+                allOf(withId(R.id.edLocationName),
+                        childAtPosition(
+                                allOf(withId(R.id.form_layout),
+                                        childAtPosition(
+                                                withClassName(is("android.support.constraint.ConstraintLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText9.perform(replaceText("USA"), closeSoftKeyboard());
+
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btGoToMap), withText("Go to Map"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        onView(withText("Fields cannot be empty!")).inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
---
+++ app/src/test/java/com/kotlinmap/andres/mapapp/SimpleTest.kt
@@ -0,0 +1,15 @@
+package com.kotlinmap.andres.mapapp
+
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Created by andres on 10/17/17.
+ */
+class SimpleTest {
+
+    @Test
+    fun unitTestingWords(){
+        assertTrue(true)
+    }
+}
